2f349ede3397e72f6484ca2816080601210f32f1
PulkitRajpal <40270691+PulkitRajpal@users.noreply.github.com>
2019-10-02 21:17:58 +0530

    Update digitalclock.c
    different approach

--- digitalclock/digitalclock.c
+++ digitalclock/digitalclock.c
@@ -3,41 +3,64 @@
 #include<util/lcd.h>
 void main()
 {
-
-DDRC=0b11111111;
+DDRD=0xFF;
 lcd_init();
-int i=0,j=0,k=0;
 while(1)
 {
-dis_cmd(0X8f);
-dis_num(i);
-
-dis_cmd(0X8c);
-dis_num(j);
-
-dis_cmd(0X89);
-dis_num(k);
-
-dis_cmd(0X8d);
-dis_string(":");
-
-dis_cmd(0X8a);
-dis_string(":");
-
-i++;
-
-_delay_ms(100);
-
-if(i==60)
-{
-	i=0;
-	j++;
-	if(j==60)
-	{j=0;k++;}
+	lcd_command(0x83);
+	lcd_string("Digital Clock");
+	_delay_ms(1);
+		lcd_command(0xcb);	
+		lcd_data(':');
+		lcd_command(0xc8);	
+		lcd_data(':');
+	
+	for(int n = 0;n<=2;n++){
+		lcd_command(0xc6);	
+		lcd_data(48+n);
+		for(int m = 0;m<9;m++){
+			lcd_command(0xc7);	
+			lcd_data(48+m);
+			if(n==2){
+			if(m==4){
+			lcd_command(0xc9);	
+			lcd_data(48+0);
+			lcd_command(0xcd);	
+			lcd_data(48+0);
+			lcd_command(0xcc);	
+			lcd_data(48+0);
+			lcd_command(0xca);	
+			lcd_data(48+0);
+			lcd_command(0xc9);	
+			lcd_data(48+0);
+			lcd_command(0xce);	
+			lcd_data(48+0);
+			
+			return;
+			}
+		}
+			for(int l = 0;l<6;l++){
+				lcd_command(0xc9);	
+				lcd_data(48+l);
+	//	_delay_ms(1000);
+				for(int k = 0;k<=9;k++){
+					lcd_command(0xca);	
+					lcd_data(48+k);
+		
+					for(int j = 0;j<6;j++){
+						lcd_command(0xcc);	
+						lcd_data(48+j);
+	//	_delay_ms(1000);
+						for(int i = 0;i<=9;i++){
+							lcd_command(0xcd);	
+							lcd_data(48+i);
+							_delay_us(1);
+						}
+					}
+				}
+			}
+		}
+	}
+}
 }
 
-
-}
-
-
-}
